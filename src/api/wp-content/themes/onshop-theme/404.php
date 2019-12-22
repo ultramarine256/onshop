e8c4960b159960c1774963a3c74f2435f3d9769f
Evgeny PLatonov <ultramarine256@gmail.com>
2019-12-22 17:06:14 +0200

    Added Api soruces (Headless WordPress).

---
+++ src/api/wp-content/themes/onshop-theme/404.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+	<div class="container">
+		<h1>
+			This page was not found
+		</h1>
+	</div>
+
+
+
+
+<?php get_footer(); ?>
---
+++ src/api/wp-content/themes/onshop-theme/app/src/assets/index.js
@@ -0,0 +1,5 @@
+// global scripts
+import './js/index';
+
+// global styles
+import './scss/index.scss';
