548ab9c822a576c5a5afeb3929e1d9876572313f
Evgeny Platonov <ultramarine256@gmail.com>
2020-01-14 21:33:22 +0200

    Removed updraftplus plugin.

--- src/api/wp-content/plugins/updraftplus/templates/wp-admin/advanced/updraftcentral.php
+++
@@ -1,3 +0,0 @@
-<?php
-if (!defined('UPDRAFTPLUS_DIR')) die('No direct access allowed');
-do_action('updraftplus_debugtools_dashboard');
--- src/api/wp-content/plugins/updraftplus/templates/wp-admin/settings/footer.php
+++
@@ -1,4 +0,0 @@
-<?php if (!defined('UPDRAFTPLUS_DIR')) die('No direct access allowed'); ?>
-<footer class="updraftplus-settings-footer">
-</footer>
-</div><!-- end #updraft-wrap -->
