5a7c0c3a0d1b35603cccce0e9f1ca54bf54d0596
Evgeny Platonov <ultramarine256@gmail.com>
2020-01-14 21:34:23 +0200

    Added angular material fro landing project.

---
+++ src/landind/src/_core/constants/scroll.constants.ts
@@ -0,0 +1,5 @@
+export const SCROLL_ANCHORS = {
+  technology: 'anchor__technology',
+  examples: 'anchor__examples',
+  aboutUs: 'anchor__about-us'
+};
--- src/landind/src/_domain/theme/footer/footer.component.ts
+++ src/landind/src/_domain/theme/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {SmoothScroll} from '../../../_core/extensions';
+import {SCROLL_ANCHORS} from '../../../_core/constants';
 
 @Component({
   selector: 'app-footer-component',
@@ -6,4 +8,15 @@ import {Component} from '@angular/core';
   templateUrl: './footer.component.html'
 })
 export class FooterComponent {
+  /// constants
+  public SCROLL_ANCHORS = SCROLL_ANCHORS;
+
+  /// constructor
+  constructor() {
+  }
+
+  /// methods
+  scrollTo(anchor: string) {
+    SmoothScroll.doScrolling(`.${anchor}`, 1000);
+  }
 }
