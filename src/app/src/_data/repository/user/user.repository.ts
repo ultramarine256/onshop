fe32b81bed4d2b933d517b8ee419639f044e940b
Valentyn Frenkel <valfrenkel@gmail.com>
2020-03-06 20:42:58 +0530

    Profile customer page implementation

--- src/app/src/_data/repository/user/user.repository.ts
+++ src/app/src/_data/repository/user/user.repository.ts
@@ -3,7 +3,7 @@ import {BaseRepository} from '../base.repository';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../../environments/environment';
 import {map} from 'rxjs/operators';
-import {ProfileModel} from './model';
+import {UserModel} from './model';
 
 @Injectable()
 export class UserRepository extends BaseRepository {
@@ -14,10 +14,10 @@ export class UserRepository extends BaseRepository {
   }
 
   /// methods
-  public getProfile() {
-    return this.httpClient.get<ProfileModel>(`${environment.apiBaseUrl}/wp-json/onshop/v1/user`)
+  public getUser() {
+    return this.httpClient.get<UserModel>(`${environment.apiBaseUrl}/wp-json/onshop/v1/user`)
       .pipe(map(x => {
-        const a = new ProfileModel();
+        const a = new UserModel();
         a.mapFromResponse(x);
         return a;
       }));
