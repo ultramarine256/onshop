53fe3620817b8c5c55e22411cd3c8e460b9e8192
elik <elik@example.comyurahi@ukr.net>
2020-03-11 17:00:31 +0200

    Refoctored code.

--- src/app/src/_domain/modules/profile/orders/orders.component.ts
+++ src/app/src/_domain/modules/profile/orders/orders.component.ts
@@ -1,7 +1,5 @@
-import {finalize} from 'rxjs/operators';
 import {Component, Input} from '@angular/core';
-import {OrderResponse, OrderRepository} from '../../../../_data';
-import {AuthService} from '../../../services';
+import {OrderResponse} from '../../../../_data';
 
 @Component({
   selector: 'app-orders-component',
@@ -9,36 +7,5 @@ import {AuthService} from '../../../services';
   templateUrl: './orders.component.html'
 })
 export class OrdersComponent {
-  /// binding
-  // @Input() phone: string;
-  public items: Array<OrderResponse> = [];
-
-  /// predicates
-  public didLoaded = false;
-  public showDescription = false;
-  public itemClicked: number;
-  public closeItem: number;
-
-  /// constructor
-  constructor(public authService: AuthService,
-              private orderRepository: OrderRepository) {
-    this.orderRepository.getOrders()
-      .pipe(finalize(() => this.didLoaded = true))
-      .subscribe((items: Array<OrderResponse>) => {
-        this.items = items;
-        console.log(items);
-      });
-  }
-
-/// methods
-  showFullDescription(id: number) {
-    this.showDescription = true;
-    this.itemClicked = id;
-  }
-
-  closeFullDescription() {
-    this.showDescription = false;
-    this.itemClicked = 0;
-  }
-
+  @Input() items: Array<OrderResponse> = [];
 }
--- src/app/src/_presentation/pages/profile/orders/orders-page.component.ts
+++ src/app/src/_presentation/pages/profile/orders/orders-page.component.ts
@@ -1,22 +1,23 @@
 import {Component} from '@angular/core';
+import {AuthService} from '../../../../_domain/services/auth';
+import {OrderRepository, OrderResponse} from '../../../../_data/repository/order';
+import {finalize} from 'rxjs/operators';
 
 @Component({
   selector: 'app-orders-page',
-  styleUrls: ['./orders-page.component.scss'],
   templateUrl: './orders-page.component.html'
 })
 export class OrdersPageComponent {
   /// fields
-  // public items: Array<OrderResponse> = [];
-  //
-  // /// predicates
-  // public didLoaded = false;
-  //
-  // /// constructor
-  // constructor(public authService: AuthService,
-  //             private orderRepository: OrderRepository) {
-  //   this.orderRepository.getOrders()
-  //     .pipe(finalize(() => this.didLoaded = true))
-  //     .subscribe((items: Array<OrderResponse>) => this.items = items);
-  // }
+  public items: Array<OrderResponse> = [];
+
+  /// spinners
+  public didLoaded = false;
+
+  constructor(public authService: AuthService,
+              private orderRepository: OrderRepository) {
+    this.orderRepository.getOrders()
+      .pipe(finalize(() => this.didLoaded = true))
+      .subscribe((items: Array<OrderResponse>) => this.items = items);
+  }
 }
