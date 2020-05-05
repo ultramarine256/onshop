edbb34ee1ddf9d0d7897d6a6eb62dda2c910017f
dmitry <mdmirty94@gmail.com>
2020-05-05 17:06:27 +0300

    Some refactoring

--- src/admin/src/_presentation/pages/app-pages.module.ts
+++ src/admin/src/_presentation/pages/app-pages.module.ts
@@ -1,27 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProjectsModule } from '../../_domain';
-import { HomePageComponent } from './home';
-import { AppPagesComponent } from './app-pages.component';
-import { AppPagesRouting } from './app-pages.routing';
-import { ProjectDetailPageComponent, ProjectPageComponent } from './project';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
+import { ProjectsModule } from '@domain/index';
+import { HomePageComponent } from './home';
+import { AppPagesComponent } from './app-pages.component';
+import { AppPagesRouting } from './app-pages.routing';
+import { ProjectDetailPageComponent, ProjectPageComponent } from './project';
+
 @NgModule({
   declarations: [AppPagesComponent, HomePageComponent, ProjectPageComponent, ProjectDetailPageComponent],
-  imports: [
-    CommonModule,
-
-    ProjectsModule,
-    MatDialogModule,
-    MatIconModule,
-
-    // app-routing
-    AppPagesRouting,
-    MatButtonModule,
-  ],
+  imports: [CommonModule, ProjectsModule, MatDialogModule, MatIconModule, MatButtonModule, AppPagesRouting],
   exports: [HomePageComponent, AppPagesComponent],
   providers: [],
 })
