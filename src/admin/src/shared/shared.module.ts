f7a67571a0eea99c4d7f2a69d7b10a068dbfde58
dmitry <mdmirty94@gmail.com>
2020-05-05 12:26:55 +0300

    Add material ui library
    Add prettier
    
    Add unsubscribe mixin
    
    Add aliases

---
+++ src/admin/src/shared/shared.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [],
+  exports: [],
+  imports: [CommonModule],
+})
+export class SharedModule {}
