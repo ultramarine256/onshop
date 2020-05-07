81a94a5e2fd3ca5fbf354d88e34a7eda9442dcd2
dmitry <mdmirty94@gmail.com>
2020-05-07 16:48:24 +0300

    Prettier, unsubscribe mixin

---
+++ src/app/src/shared/utils/unsubscribe-mixin.ts
@@ -0,0 +1,14 @@
+import { OnDestroy } from '@angular/core';
+import { Subject } from 'rxjs';
+
+type Constructor<T> = new (...args: any[]) => T;
+
+export function UnsubscribeMixin<T extends Constructor<{}>>(Base: T = class {} as any) {
+  return class extends Base implements OnDestroy {
+    destroy$ = new Subject<void>();
+
+    ngOnDestroy() {
+      this.destroy$.next();
+    }
+  };
+}
