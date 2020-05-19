cd553048b10371b5acbb348f4976e87a41b92c1b
dmitry <mdmirty94@gmail.com>
2020-05-19 12:13:22 +0300

    Navigation refactoring

--- src/app/src/_domain/modules/_theme/header/header.module.ts
+++ src/app/src/_domain/modules/_theme/header/header.module.ts
@@ -1,26 +1,27 @@
-import {NgModule} from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {CommonModule} from '@angular/common';
-import {HeaderComponent} from './header.component';
-import {ProductSearchModule} from '../products-search';
-import {RouterModule} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+import { RouterModule } from '@angular/router';
+
+import { ThirdPartyModule } from '@domain/modules/_theme/_third-party';
+import { HeaderComponent } from './header.component';
+import { ProductSearchModule } from '../products-search';
 
 @NgModule({
-  declarations: [
-    HeaderComponent
-  ],
+  declarations: [HeaderComponent],
   imports: [
     FormsModule,
     ReactiveFormsModule,
     CommonModule,
     RouterModule,
 
+    // Third parties
+    ThirdPartyModule,
+
     /// theme
-    ProductSearchModule
+    ProductSearchModule,
   ],
-  exports: [
-    HeaderComponent
-  ]
+  exports: [HeaderComponent],
 })
-export class HeaderModule {
-}
+export class HeaderModule {}
