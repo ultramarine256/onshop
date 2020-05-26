2db4a900729ab7456df82f7b6adbf0385c6f9962
dmitry <mdmirty94@gmail.com>
2020-05-26 15:40:09 +0300

    Search result page

--- src/app/src/_domain/modules/_theme/products-search/products-search.component.ts
+++ src/app/src/_domain/modules/_theme/products-search/products-search.component.ts
@@ -1,34 +1,40 @@
-import {Component, OnInit} from '@angular/core';
-import {FormControl} from '@angular/forms';
-import {debounceTime, switchMap, takeUntil, tap} from 'rxjs/operators';
-import {UnsubscribeMixin} from '@shared/utils/unsubscribe-mixin';
-import {ProductFilter, ProductModel, ProductRepository} from '@data/repository';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { debounceTime, map, switchMap, tap } from 'rxjs/operators';
+
+import { ProductFilter, ProductModel, ProductRepository } from '@data/repository';
 
 @Component({
   selector: 'app-products-search',
   styleUrls: ['./products-search.component.scss'],
   templateUrl: './products-search.component.html',
 })
-export class ProductSearchComponent extends UnsubscribeMixin() implements OnInit {
-  public searchResults: Array<ProductModel> = [];
-  public products: ProductModel[];
-  public searchBar = new FormControl();
+export class ProductSearchComponent implements OnInit {
   public isLoading = false;
 
-  constructor(private productRepository: ProductRepository) {
-    super();
-  }
+  public products$: Observable<ProductModel[]>;
+  public searchBar = new FormControl();
+
+  constructor(private router: Router, private productRepository: ProductRepository) {}
 
   ngOnInit() {
-    this.searchBar.valueChanges
-      .pipe(
-        debounceTime(300),
-        tap(() => (this.isLoading = true)),
-        switchMap((search) =>
-          this.productRepository.getProducts(new ProductFilter({search})).pipe(tap(() => (this.isLoading = false)))
-        )
-      )
-      .pipe(takeUntil(this.destroy$))
-      .subscribe((res) => (this.searchResults = res.items));
+    this.products$ = this.searchBar.valueChanges.pipe(
+      debounceTime(300),
+      tap(() => (this.isLoading = true)),
+      switchMap((search) => {
+        return (search ? this.productRepository.getProducts(new ProductFilter({ search })) : of({ items: [] })).pipe(
+          tap(() => (this.isLoading = false))
+        );
+      }),
+      map((productSearchResult) => productSearchResult.items)
+    );
+  }
+
+  search() {
+    const term = this.searchBar.value;
+    this.searchBar.setValue('');
+    this.router.navigate(['/search'], { queryParams: { term } });
   }
 }
