1be53aec30fe88e1a967e35705b5c38de2896e05
dmitry <mdmirty94@gmail.com>
2020-06-23 18:47:07 +0300

    Remove php warnings

--- src/api/wp-config.php
+++ src/api/wp-config.php
@@ -133,7 +133,10 @@ if ( $wp_debug ) {
 	define( 'WP_DEBUG_LOG', true );
 	define( 'WP_DEBUG_DISPLAY', true );
 } else {
-	define( 'WP_DEBUG', false );
+    define('WP_DEBUG', false);
+    ini_set('display_errors','Off');
+    ini_set('error_reporting', E_ALL );
+    define('WP_DEBUG_DISPLAY', false);
 }
 
 /* That's all, stop editing! Happy publishing. */
